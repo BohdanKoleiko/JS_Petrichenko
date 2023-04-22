03321f8479cfde0f1346d3494a34f914b90602d0
HamaHs <lusuy1995@gmail.com>
2023-04-22 21:59:44 +0200

    the tasks are solved by me

--- Practice_2/js/script.js
+++ Practice_2/js/script.js
@@ -24,3 +24,38 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    promoBg = document.getElementsByClassName('promo__bg'),
+    moviesList = document.querySelector('.promo__interactive-list'),
+    bgImg = 'img/bg.jpg';
+
+if (adv) {
+    adv.forEach(item => {
+        item.remove()
+    });
+}
+
+if (promoBg) {
+    for (let i = 0; i < promoBg[0].children.length; i++) {
+        if (promoBg[0].children[i].className === 'promo__genre')
+            promoBg[0].children[i].innerText = 'драма';
+    }
+    promoBg[0].style.cssText = `background-image: url('${bgImg}');`;
+}
+
+if (moviesList) {
+    const sortedMovies = movieDB.movies.sort();
+    moviesList.innerHTML = '';
+
+    sortedMovies.forEach((film, i) => {
+        moviesList.innerHTML += `
+            <li class="promo__interactive-item">
+                ${i + 1}) ${film}
+                <div class="delete"></div>
+            </li>
+        `
+    })
+}
+
+const evt = new Event("look", { "bubbles": true, "cancelable": false });
+document.dispatchEvent(evt);

6f4337cf69688289558bb7826a59fae57e31e68e
HamaHs <lusuy1995@gmail.com>
2023-08-04 13:08:08 +0200

    add check of zero and minus integer and 'Property Value Shorthand' to the getTimeRemaining function, added a check to clreatInterval works, and created getZeto function

--- Practice_4/4.2_create-timer/dist/js/script.js
+++ Practice_4/4.2_create-timer/dist/js/script.js
@@ -66,20 +66,36 @@ const timeCounter = function () {
   //const deadline = function (endDay) {
   //   return today.setDate(today.getDate() + endDay)
   //}
-  const deadline = '2023-08-04';
+  const deadline = '2023-08-20';
   const getTimeRemaining = function (endtime) {
-    const t = Date.parse(endtime) - Date.now() < 0 ? 0 : Date.parse(endtime) - Date.now(),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor(t / (1000 * 60 * 60) % 24),
-      minutes = Math.floor(t / 1000 / 60 % 60),
+    const t = Date.parse(endtime) - Date.now();
+    let days = 0,
+      hours = 0,
+      minutes = 0,
+      seconds = 0;
+    if (t > 0) {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor(t / (1000 * 60 * 60) % 24);
+      minutes = Math.floor(t / 1000 / 60 % 60);
       seconds = Math.floor(t / 1000 % 60);
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
+    }
+    const time = {
+      total: t,
+      days,
+      hours,
+      minutes,
+      seconds
     };
+    return time;
+  };
+  const getZero = num => {
+    if (!isNaN(num) || num || num > 0) {
+      if (num < 10) {
+        return `0${num}`;
+      } else {
+        return num;
+      }
+    }
   };
   const setClock = function (selector, endTime) {
     const timer = document.querySelector(selector),
@@ -90,11 +106,11 @@ const timeCounter = function () {
       timeInterval = setInterval(updateClock, 1000);
     function updateClock() {
       const t = getTimeRemaining(endTime);
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
-      if (!t.total) {
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+      if (t.total < 0 || !t.total) {
         clearInterval(timeInterval);
       }
     }

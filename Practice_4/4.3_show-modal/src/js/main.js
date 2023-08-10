f9bc6006e0f429991a310ab270171e2083d05dbf
HamaHs <lusuy1995@gmail.com>
2023-08-10 20:36:48 +0200

    removed

--- Practice_4/4.3_show-modal/src/js/main.js
+++
@@ -1,9 +0,0 @@
-import tabs from "./tabs";
-import timeCounter from "./timeCounter";
-import modal from "./modal";
-
-window.addEventListener("DOMContentLoaded", () => {
-   tabs();
-   timeCounter();
-   modal();
-});

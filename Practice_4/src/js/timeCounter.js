e726e0f1ba2f6e1517694ac6ad81023c03e569b8
HamaHs <lusuy1995@gmail.com>
2023-09-19 10:28:45 +0200

    added some description

--- Practice_4/src/js/timeCounter.js
+++ Practice_4/src/js/timeCounter.js
@@ -4,19 +4,36 @@ const timeCounter = function () {
    //const deadline = function (endDay) {
    //   return today.setDate(today.getDate() + endDay)
    //}
-   const deadline = '2023-08-20';
+   const deadline = '2023-08-28';
 
    const getTimeRemaining = function (endtime) {
+      // Перший крок
       const t = Date.parse(endtime) - Date.now();
+      // the first we parse a data of "endtime" variable to get the result in a milliseconds
+      // then we get the result in milliseconds of the current day and hours
+      // then we subtract(відняти) milliseconds of current day from milliseconds of the last day
+      // then assign execution of expression to 't' variable
+
       let days = 0,
          hours = 0,
          minutes = 0,
          seconds = 0;
 
       if (t > 0) {
-         days = Math.floor(t / (1000 * 60 * 60 * 24));
-         hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+         // Другий крок
+         // to get days, hours, minutes and seconds we need to transform millisecods
+         days = Math.floor(t / 1000 / 60 / 60 / 24);
+         // 1000 is a number of milliseconds in a second
+         // 60 is a number of seconds in a minute
+         // next 60 is a number of minutes in an hour
+         // 24 is a number of hours in a day
+
+         hours = Math.floor((t / 1000 / 60 / 60) % 24);
+         // here we use % operator to get whole part from (t / 1000 / 60 / 60) result but not higher than 24 hourse per day
+
          minutes = Math.floor((t / 1000 / 60) % 60);
+         // the same as above but now we should get minutes so we use divide з остатком на 60 because we have 60 minutes in an hour
+
          seconds = Math.floor((t / 1000) % 60);
       }
 
@@ -46,14 +63,14 @@ const timeCounter = function () {
       function updateClock() {
          const t = getTimeRemaining(endTime);
 
+         if (t.total < 0 || !t.total) {
+            clearInterval(timeInterval);
+         }
+
          days.innerHTML = getZero(t.days);
          hours.innerHTML = getZero(t.hours);
          minutes.innerHTML = getZero(t.minutes);
          seconds.innerHTML = getZero(t.seconds);
-
-         if (t.total < 0 || !t.total) {
-            clearInterval(timeInterval);
-         }
       }
 
       updateClock();

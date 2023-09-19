eec04d061c615fd878d3df58be2bc9a209a6ba65
HamaHs <lusuy1995@gmail.com>
2023-09-19 10:52:01 +0200

    practice to work with windows in js

--- Lessons/71_windowsAndWorkWithThem/script.js
+++ Lessons/71_windowsAndWorkWithThem/script.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
    const scrollBlock = document.querySelector(".scroll-txt");
    const btn = document.querySelector('button');
 
+   //console.log(scrollBlock.clientWidth);
+   //console.log(scrollBlock.offsetWidth);
+   console.log(scrollBlock.scrollHeight);
+
    btn.addEventListener('click', () => {
+      scrollBlock.scrollTop = 0;
       console.log(`scrollTop: ${scrollBlock.scrollTop}`);
    });
 });

268ca477ea698d65c49ca270ff02b52b34b72d4d
HamaHs <lusuy1995@gmail.com>
2025-04-01 00:05:48 -0400

    stop watching for files

--- Lessons/Recursion/script.js
+++
@@ -1,9 +0,0 @@
-function getPropOfObj(obj) {
-   for (let key in obj) {
-      if (typeof obj[key] !== 'object') {
-         console.log(`Property ${key} has a value ${obj[key]}`);
-      } else {
-         getPropOfObj(obj[key]);
-      }
-   }
-}

f92cb2d84d531002a5805a179a95a47bd4e4452b
HamaHs <lusuy1995@gmail.com>
2025-04-06 00:00:52 -0400

    JS data types is considered

---
+++ 2-data-types-in-js/index.js
@@ -0,0 +1,18 @@
+// Primitives
+const numType = 0;
+const stringType = "string";
+const booleanType = true / false;
+const undefinedType = undefined;
+const nullType = null;
+const bigintType = BigInt;
+const symbolType = Symbol;
+
+// Objects
+const objectType = {
+   name: "Bohdan",
+   age: 29,
+};
+const arrayAsAnObjectType = [25, "simple string", false];
+const functionAsAnObjectType = () => {
+   return "I'm function";
+};
